810517d5afd5bc55a05f6068843c907be8759105
eduard0915 <edd.alvis@gmail.com>
2026-06-06 15:01:27 -0500

    Correction bug and footer

--- app.py
+++ app.py
@@ -24,7 +24,6 @@ app.config['MAIL_PORT'] = config('MAIL_PORT')
 app.config['MAIL_USE_TLS'] = config('MAIL_USE_TLS')
 app.config['MAIL_USERNAME'] = config('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = config('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = config('EMAIL_HOST_USER')
 
 mail = Mail(app)
 
